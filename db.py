659d9e9de152c5f661dc76b3348ca33a01f311f9
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-27 17:14:51 -0400

    Persist messages

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import redis
+import redis.asyncio as redis
 
 
 class Redis:
@@ -6,3 +6,10 @@ class Redis:
 
     def connect(self, url: str):
         self._conn = redis.Redis.from_url(url)
+
+    async def add_message(self, message: str):
+        await self._conn.rpush("messages", message)
+
+    async def get_messages(self) -> str:
+        messages = await self._conn.lrange("messages", 0, -1)
+        return messages
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 
 from db import Redis
@@ -10,8 +10,12 @@ from ws import ConnectionManager
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-r = Redis()
-r.connect(os.environ["REDIS_URL"])
+dbconn = Redis()
+try:
+    dbconn.connect(os.environ["REDIS_URL"])
+except:
+    print("Unable to connect to Redis")
+    exit(1)
 
 manager = ConnectionManager()
 
@@ -21,12 +25,19 @@ def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+@app.get("/messages", response_class=JSONResponse)
+async def messages():
+    messages = await dbconn.get_messages()
+    return messages
+
+
 @app.websocket("/ws")
 async def ws(ws: WebSocket):
     await manager.connect(ws)
     try:
         while True:
             message = await ws.receive_text()
+            await dbconn.add_message(message)
             await manager.broadcast(message)
     except WebSocketDisconnect:
         manager.disconnect(ws)
